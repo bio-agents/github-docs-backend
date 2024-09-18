65122074ecd6a57a3a4bae64d2eae69118d73fd8
johnson.chen <joy6677@qq.com>
2024-09-18 12:11:14 +0800

    Update content and rename

---
+++ src/main/java/com/mycompany/documentation/api/Api.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Agents | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.documentation.api;
+
+import com.mycompany.documentation.api.logic.Constants;
+import com.mycompany.documentation.model.Links;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+/**
+ *
+ * @author Vicky Sundesha <vicky.sundesha@bsc.es>
+ */
+@Path("/")
+public class Api {
+    
+    /**
+     * Returns the list of repositories for the menu of the documentation page
+     * @param repolist the name of the repolist eg: oeb, wg .. etc
+     * @return list of repositories
+     */
+    @GET
+    @Path("/{repolist}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String,String> repoListToHtml(@PathParam("repolist") String repolist){
+        Map<String,String> list = Constants.oebRepos;
+        if(!repolist.isEmpty()){
+            switch (repolist){
+                case "oeb":
+                    list = Constants.oebRepos;
+                    break;
+                case "wg":
+                    list = Constants.widgetGalleryRepos;
+                    break;
+                case "vre":
+                    list = Constants.vre;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return list;
+    };
+    
+    /**
+     * Returns list of projects for the documentation hub.
+     * 
+     */
+    @GET
+    @Path("/projects")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ArrayList<Links> getProjects(){
+        return Constants.projects;
+    };
+    
+
+}
+
